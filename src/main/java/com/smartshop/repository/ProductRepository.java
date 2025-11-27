0d05c7ec386bf22516af080b2ee60a589d2c1c9a
ichrakjaifra <ichrak.jaifra@gmail>
2025-11-27 16:42:22 +0100

    SMAR-16 CRUD complet des produits

---
+++ src/main/java/com/smartshop/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.smartshop.repository;
+
+import com.smartshop.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByDeletedFalse();
+
+    @Query("SELECT p FROM Product p WHERE p.deleted = false AND p.stockDisponible > 0")
+    List<Product> findAvailableProducts();
+
+    List<Product> findByNomContainingIgnoreCaseAndDeletedFalse(String nom);
+}
